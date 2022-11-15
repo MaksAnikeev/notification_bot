b3986f32e1871e7022db19212cc3292eb7cdf574
MaksAnikeev <anikeev.maks@rambler.ru>
2022-11-15 07:47:48 +0300

    Clean code

--- notification_bot.py
+++ notification_bot.py
@@ -1,22 +1,11 @@
+import argparse
 import os
 import time
 
 import requests
 from dotenv import load_dotenv
 from telegram import Bot
-from telegram.ext import CommandHandler, Updater
-
-
-# def start(update, context):
-#     user_fullname = str(update.message.from_user['first_name']) + ' ' + str(
-#         update.message.from_user['last_name'])
-#
-#     context.bot.send_message(
-#         chat_id=update.effective_chat.id,
-#         text=f'Здравствуйте {user_fullname}. Этот бот будет контролировать'
-#              f'статус проверки сданных вами работ на платформе Dewman'
-#     )
-#     return check_status_lesson_verification(update, context, url=url)
+from telegram.ext import Updater
 
 
 def send_text(chat_id, response_params):
@@ -45,15 +34,17 @@ def check_status_lesson_verification(chat_id, url):
                 "Authorization": devman_token
             }
             params = {'timestamp': timestamp}
-            response = requests.get(url,
-                                    headers=headers,
-                                    params=params,
-                                    timeout=10)
+            response = requests.get(
+                url,
+                headers=headers,
+                params=params,
+                timeout=10)
             response.raise_for_status()
             response_params = response.json()
             if response_params['status'] == 'found':
-                send_text(chat_id=chat_id,
-                          response_params=response_params)
+                send_text(
+                    chat_id=chat_id,
+                    response_params=response_params)
                 timestamp = response_params['last_attempt_timestamp']
             elif response_params['status'] == 'timeout':
                 timestamp = response_params['timestamp_to_request']
@@ -70,21 +61,21 @@ def check_status_lesson_verification(chat_id, url):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'chat_id',
+        type=int,
+        help='chat_id вашего телеграма')
+    args = parser.parse_args()
+
     load_dotenv()
-    token = os.getenv("TG_BOT_TOKEN")
-    devman_token = os.getenv("DEVMAN_TOKEN")
+    token = os.getenv('TG_BOT_TOKEN')
+    devman_token = os.getenv('DEVMAN_TOKEN')
 
     url = 'https://dvmn.org/api/long_polling/'
 
     bot = Bot(token=token)
     updater = Updater(token=token, use_context=True)
-    # dispatcher = updater.dispatcher
 
-    chat_id = 704859099
-    check_status_lesson_verification(chat_id=chat_id, url=url)
+    check_status_lesson_verification(chat_id=args.chat_id, url=url)
 
-    # start_handler = CommandHandler('start', start)
-    # dispatcher.add_handler(start_handler)
-    #
-    # updater.start_polling()
-    # updater.idle()
